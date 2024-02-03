58f89daaf6ab271687ef1fc12d2c379b7cd6e8fb
erty2134 <olly.lansdown@gmail.com>
2024-02-03 20:03:01 +1100

    permately fixed the token issue
    - fixed token issue by encrypting it kinda :\
    - thats about it, i suppose

--- main.py
+++ main.py
@@ -7,17 +7,23 @@ import os
 from dotenv import find_dotenv, load_dotenv
 
 
+
+
 def main(argv:"list", argc:"int", *args:"any", **kwargs:"any") -> None:
 
     # load up the env
     envPath = find_dotenv();
     load_dotenv(envPath);
     TOKEN = os.getenv("ENV_TOKEN");
-
+    TOKEN = TOKEN.strip("_*)(/")
 
     client = discord.Client(intents=discord.Intents.default());
 
     client.run(TOKEN);
+
+    #async def welcome(ctx) -> None:
+    #    await ctx.reply(f"Hello, {ctx}");
+
 
 
 if __name__ == "__main__": main(sys.argv[1:], len(sys.argv[1:]));
