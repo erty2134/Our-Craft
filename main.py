cef56cde6ce76a1997ba9e0b9fa2b4907f50adc8
erty2134 <olly.lansdown@gmail.com>
2024-02-03 14:17:23 +1100

    added basic stuff i guess

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 # main.py
 
+import sys
 import discord
 
 
 
+def main(argv, argc, *args, **kwargs) -> None:
+    client = discord.Client(intents=discord.Intents.default());
 
-client = discord.Client(intents=discord.Intents.default());
+    client.run("MTIwMzEzMTQ1MjM1MTU3ODEzMw.GS8r95.WNmzTab7qBshJvVrsb5XaPKzUlVR1xDB3u1g5E");
 
-client.run("MTIwMzEzMTQ1MjM1MTU3ODEzMw.GS8r95.WNmzTab7qBshJvVrsb5XaPKzUlVR1xDB3u1g5E");
+
+if __name__ == "__main__": main(sys.argv[1:], len(sys.argv[1:]));
