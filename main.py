778d301a687d03a515032555cc7fb05e8c5281bf
erty2134 <149846282+erty2134@users.noreply.github.com>
2024-02-03 13:58:26 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# main.py
+
+import discord
+
+
+
+
+client = discord.Client(intents=discord.Intents.default());
+
+client.run("MTIwMzEzMTQ1MjM1MTU3ODEzMw.GS8r95.WNmzTab7qBshJvVrsb5XaPKzUlVR1xDB3u1g5E");
